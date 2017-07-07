a990e60064da8fb8d7247a456cd5221d8b7cc1bd
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2017-07-07 14:56:22 +0200

    Missing Exception class added.

---
+++ src/Exception.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @author Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
+ * @copyright 2010-2017 JTL-Software GmbH
+ */
+namespace jtl\Connector\Client;
+
+class Exception extends \Exception
+{
+
+}
