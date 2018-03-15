5403d4b8b05efcaacb4138d1875955ef8aff9baa
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2018-03-15 14:16:05 +0100

    Connector core updated.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 require_once dirname(__DIR__) . '/vendor/autoload.php';
+\Doctrine\Common\Annotations\AnnotationRegistry::registerLoader('class_exists');
+
+
 /**
  * @author Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
  * @copyright 2010-2017 JTL-Software GmbH
