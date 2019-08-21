e9c3764b6699d963c63281307c904493ab73c841
Immanuel Klinkenberg <immanuel.klinkenberg@jtl-software.com>
2019-08-21 09:03:00 +0200

    License corrected.

--- tests/src/ClientTest.php
+++ tests/src/ClientTest.php
@@ -13,49 +13,5 @@ use PHPUnit\Framework\TestCase;
 
 class ClientTest extends TestCase
 {
-
-    public function testPush()
-    {
-        $this->markTestIncomplete('not implemented yet!');
-    }
-
-    public function testIsAuthenticated()
-    {
-        $this->markTestIncomplete('not implemented yet!');
-    }
-
-    public function testPull()
-    {
-        $this->markTestIncomplete('not implemented yet!');
-    }
-
-    public function testAck()
-    {
-        $this->markTestIncomplete('not implemented yet!');
-    }
-
-    public function testIdentify()
-    {
-        $this->markTestIncomplete('not implemented yet!');
-    }
-
-    public function testFeatures()
-    {
-        $this->markTestIncomplete('not implemented yet!');
-    }
-
-    public function testFinish()
-    {
-        $this->markTestIncomplete('not implemented yet!');
-    }
-
-    public function testClear()
-    {
-        $this->markTestIncomplete('not implemented yet!');
-    }
-
-    public function testDelete()
-    {
-        $this->markTestIncomplete('not implemented yet!');
-    }
+    
 }
